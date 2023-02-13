41e04e59d40baaa633ff1daca6a75a3355f241a6
Yaman Kalaji <syk.yaman@gmail.com>
2023-02-13 17:03:03 +0000

    Successfully showed a polygon on the map

--- Code/sensor_sight_app/lib/main.dart
+++ Code/sensor_sight_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -13,10 +15,39 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(51.509865, -0.118092);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  Set<Polygon> _polygon = HashSet<Polygon>();
+
+  // created list of locations to display polygon
+  List<LatLng> points = [
+    LatLng(51.52303429074671, -0.15491336535643718),
+    LatLng(51.53275609139346, -0.16632884692382),
+    LatLng(51.535642529145015, -0.15079349230956218),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _polygon.add(Polygon(
+      // given polygonId
+      polygonId: PolygonId('1'),
+      // initialize the list of points to display polygon
+      points: points,
+      // given color to polygon
+      fillColor: Colors.green.withOpacity(0.3),
+      // given border color to polygon
+      strokeColor: Colors.green,
+      geodesic: true,
+      // given width of border
+      strokeWidth: 4,
+    ));
   }
 
   @override
@@ -33,6 +64,7 @@ class _MyAppState extends State<MyApp> {
             target: _center,
             zoom: 11.0,
           ),
+          polygons: _polygon,
         ),
       ),
     );
