5389c3d7f973358d76da0a27bd374155c2ea22b7
Yaman Kalaji <syk.yaman@gmail.com>
2023-03-17 23:11:25 +0000

    Fixed the persistent error in MySQL connection & add vision field coordinates to the API

--- Code/web_api/controllers/camerasController.js
+++ Code/web_api/controllers/camerasController.js
@@ -4,24 +4,37 @@ export default class camerasController {
   constructor() {
   }
 
- async getAll(req, res) {
-    try{
       var con = mysql.createConnection({
         host: "host",
         user: "",
         password: "",
         database: ""
+  async getAll(req, res) {
+    try {
       });
 
-      con.connect(function(err) {
-        if (err) throw err;
-        con.query("SELECT * FROM cameras", function (err, result, fields) {
-          if (err) throw err;
-          console.log(result);
-          res.send(result);
-        });
-      });
+      connection.connect();
       
+      connection.query("SELECT * FROM cameras", function (err, result, fields) {
+        if (err) {
+          console.log(err);
+          res.sendStatus(500);
+        }
+        console.log(result);
+        
+        for (var i = 0; i < result.length; i++) {
+          result[i].camera_coordinates = JSON.parse(result[i].camera_coordinates);
+          result[i].column_base_coordinates = JSON.parse(result[i].column_base_coordinates);
+          result[i].vision_field_1 = JSON.parse(result[i].vision_field_1);
+          result[i].vision_field_2 = JSON.parse(result[i].vision_field_2);
+          result[i].vision_field_3 = JSON.parse(result[i].vision_field_3);
+          result[i].vision_field_4 = JSON.parse(result[i].vision_field_4);
+        }
+
+        res.send(result);
+      });
+
+      connection.end();
 
     } catch (err) {
       console.log(err);
