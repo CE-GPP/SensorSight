4c73eb343adbefdbd153ea56ec18330d1e3912da
Haiden McGill <46386953+heyhaiden@users.noreply.github.com>
2023-03-22 16:34:39 +0000

    Updated about popup

--- Code/sensor_sight_app/lib/widgets/app_info.dart
+++ Code/sensor_sight_app/lib/widgets/app_info.dart
@@ -9,7 +9,8 @@ class AppInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
         title: const Text('About the App'), // change title
-        content: const Text('This app is a demo app for SensorSight.'),
+        content: const Text(
+            'SensorSight is a BI visualisation tool for outdoor CCTV planning and management for parks and private property. \n\nTechnicians can easily service devices using a 2D map and geospatial AR view of the camera FOV.'),
         actions: <Widget>[
           TextButton(
             onPressed: () => Navigator.pop(context),
